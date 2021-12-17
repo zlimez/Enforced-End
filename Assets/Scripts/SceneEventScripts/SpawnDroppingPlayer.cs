39f19c6a8483aec0ee85932b93075ef9e6684887
Ger Hean <gerhean@live.com>
2021-12-17 18:41:37 +0800

    Add dropping player

--- Assets/Scripts/SceneEventScripts/SpawnDroppingPlayer.cs
+++ Assets/Scripts/SceneEventScripts/SpawnDroppingPlayer.cs
@@ -16,4 +16,16 @@ public class SpawnDroppingPlayer : MonoBehaviour
         GameObject fallingPlayer = Instantiate(droppingPlayerPrefab, transform.position, Quaternion.identity);
         fallingPlayer.GetComponent<ThrownMinion>().InitializeFallOnly(20.0f);
     }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            this.GetComponent<SpriteRenderer>().enabled = true;
+            StartCoroutine(DieAfterSeconds(0.5f));
+        }
+    }
+
+    IEnumerator DieAfterSeconds(float seconds) {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
 }
